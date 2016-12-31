f4b391c62a3bb0f81999243c2226669b75ccf3a3
yapingxin <xinyp@live.com>
2016-12-31 21:52:23 +0800

    Localized Display Demo

---
+++ Daily/2016/1231/DisplayChineseText/Win32_VC2015/CodeWarrior/Localized-Display/Localized-Display.cpp
@@ -0,0 +1,17 @@
+// Localized-Display.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <locale.h>
+
+int main()
+{
+	static char ctxt[] = "Display:";
+	static wchar_t wtxt[] = L"简体中文的世界。";
+
+	setlocale(LC_CTYPE, "chs");
+
+	printf("Display: %ls\n", wtxt);
+    return 0;
+}
+
