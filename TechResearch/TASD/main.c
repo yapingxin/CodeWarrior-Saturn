0ff339ef85b7467f06f165d459b997c1e50fe0e0
YapingXin <xinyp@live.com>
2017-01-09 07:11:58 +0800

    TechResearch

---
+++ TechResearch/TASD/main.c
@@ -0,0 +1,70 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+int main()
+{
+    /* Our process ID and Session ID */
+    pid_t pid, sid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    if (pid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0)
+    {
+        /* exit the parent process */
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Change the file mode mask */
+    umask(0);
+
+    /* Open any logs here */
+
+    /* Create a new SID for the child process */
+    sid = setsid();
+
+    if (sid < 0)
+    {
+        /* Log the failure */
+        exit(EXIT_FAILURE);
+    }
+
+    /* Change the current working directory */
+    if ((chdir("/")) < 0)
+    {
+        /* Log the failure */
+        exit(EXIT_FAILURE);
+    }
+
+    /* Close out the standard file descriptors */
+    //close(STDIN_FILENO);
+    //close(STDOUT_FILENO);
+    //close(STDERR_FILENO);
+
+    /* Daemon-specific initialization goes here */
+
+    printf("Echo from the child process. process id is: %d\n", getpid());
+
+    /* The Big Loop */
+    while (1)
+    {
+        /* Do some task here ... */
+        sleep(30); /* wait 30 seconds */
+
+        printf("Echo\n");
+    }
+
+    return 0;
+}
